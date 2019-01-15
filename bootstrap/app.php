002e9d88135ea85a930130d7c9e895db5de7c97c
Alexey Dobrynin <ag.dobrynin@gmail.com>
2019-01-15 14:28:19 +0400

    Finish adding Twig template

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -8,4 +8,20 @@ $app = new Slim\App([
     ],
 ]);
 
+$container = $app->getContainer();
+/** @var \Slim\Views\Twig $container['view'] */
+$container['view'] = function ($container) {
+    $view = new Slim\Views\Twig(__DIR__ . '/../resources/views', [
+        'cache' => false,
+
+    ]);
+
+    $view->addExtension(new Slim\Views\TwigExtension(
+        $container->router,
+        $container->request->getUri()
+    ));
+
+    return $view;
+};
+
 require __DIR__ . '/../app/routes.php';
