a36de991c7e066221175aa2dc36c296afd644650
Alexey Dobrynin <ag.dobrynin@gmail.com>
2019-01-16 16:34:22 +0400

    Add middleware for old input for show exist data in form

---
+++ app/Middleware/OldInputMiddleware.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Middleware;
+
+use Slim\Http\Request;
+use Slim\Http\Response;
+
+class OldInputMiddleware extends Middleware
+{
+    public function __invoke(Request $request, Response $response, $next)
+    {
+        $this->container->view->getEnvironment()->addGlobal('old', $_SESSION['old'] ?? null);
+        $_SESSION['old'] = $request->getParams();
+        return $next($request, $response);
+    }
+}
