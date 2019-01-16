28aeb2587cd175bef8b5387f89fe2e1881e26409
Alexey Dobrynin <ag.dobrynin@gmail.com>
2019-01-17 01:27:55 +0400

    Change password functionality

---
+++ app/Controllers/Auth/PasswordController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Controllers\Auth;
+
+use App\Controllers\Controller;
+use Slim\Http\Request;
+use Slim\Http\Response;
+use Respect\Validation\Validator as V;
+
+class PasswordController extends Controller
+{
+    public function getChange(Request $request, Response $response)
+    {
+        return $this->view->render($response, 'auth/change/password.twig');
+    }
+
+    public function postChange(Request $request, Response $response)
+    {
+        $validator = $this->validator->validate($request, [
+            'password_old' => [
+                'rules' => V::matchesPassword($this->auth->user()->password),
+                'message' => 'Текущий пароль неверный!'
+            ],
+            'password' => [
+                'rules' => V::length(6, null)
+                            ->noWhitespace(),
+                'messages' => [
+                    'length' => 'Длина пароля не менее 6 символов',
+                    'noWhitespace' => 'Пароль содержит символ "пробел"',
+                ]
+            ],
+            'confirm_password' => [
+                'rules' => V::equals($request->getParam('password')),
+                'message' => 'Пароли не совпадают',
+            ],
+        ]);
+
+        // УПС - валадация сломана :-(
+        if (!$validator->isValid()) {
+            return $this->view->render($response, 'auth/change/password.twig');
+        }
+
+        $this->auth->user()->setPassword($request->getParam('password'));
+
+        $this->flash->addMessage('info', 'Пароль успешно изменен');
+        return $response->withStatus(302)->withRedirect($this->router->pathFor('home'));
+    }
+}
--- app/routes.php
+++ app/routes.php
@@ -8,3 +8,6 @@ $app->post('/auth/signup', 'AuthController:postSignUp');
 $app->get('/auth/signin', 'AuthController:getSignIn')->setName('auth.signin');
 $app->post('/auth/signin', 'AuthController:postSignIn');
 $app->get('/auth/signout', 'AuthController:getSignOut')->setName('auth.signout');
+
+$app->post('/auth/password/change', 'PasswordController:postChange');
+$app->get('/auth/password/change', 'PasswordController:getChange')->setName('auth.password.change');
